727fc83abb1a5395bac35036936dcbb65689b273
Hamza Umar Farooq Ghani <hamzaumarfarooqghani@rentalcars.com>
2020-08-14 20:47:05 +0100

    first commit

---
+++ src/test/java/io/github/hufghani/BPDTS/BpdtsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.hufghani.BPDTS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BpdtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
