c355a9c7a2a7c64a9ff6d59013341f0dd880ddf1
Hamza Umar Farooq Ghani <hamzaumarfarooqghani@rentalcars.com>
2020-08-15 17:25:14 +0100

    fix import

--- src/test/java/io/github/hufghani/BPDTS/controller/UserControllerTest.java
+++ src/test/java/io/github/hufghani/BPDTS/controller/UserControllerTest.java
@@ -1,27 +1,21 @@
 package io.github.hufghani.bpdts.controller;
 
 
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import io.github.hufghani.bpdts.enity.User;
 import io.github.hufghani.bpdts.service.UserService;
 import java.util.ArrayList;
-
 import java.util.List;
 import org.junit.jupiter.api.Test;
-
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-
-import static org.hamcrest.Matchers.containsString;
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
 class UserControllerTest {
